f36147eaefe98cc8e6c108409a03de7a4a173382
Minahil Ghafoor <ReadyForIt7890@gmail.com>
2026-01-09 20:32:57 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ClientApp;
+using System.Net.Http;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5088/") });
+
+await builder.Build().RunAsync();
